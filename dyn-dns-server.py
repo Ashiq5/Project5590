8663b9a7b57612af2811942888271d9c5b83bbc9
ashiq <ishtiaqashiq5@gmail.com>
2022-05-01 16:56:10 -0400

    first commit

--- dyn-dns-server.py
+++ dyn-dns-server.py
@@ -181,7 +181,7 @@ def main():
 
 
 if __name__ == '__main__':
-    for i in range(1, 101):
+    for i in range(1, 21):
         dom = 'fake-' + str(i) + '.' + victim_domain
         referral_responses.append(NS(dom))
     main()
