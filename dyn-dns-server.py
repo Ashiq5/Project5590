37a36bb3f6c456f04be78139a0a18eedeb29ee1b
ashiq <ishtiaqashiq5@gmail.com>
2022-05-02 21:54:17 -0400

    first commit

--- dyn-dns-server.py
+++ dyn-dns-server.py
@@ -83,7 +83,7 @@ def dns_response(data, client_ip):
         val = 0  # random.choice([0, 1])
         if val == 0:
             for rdata in referral_responses:
-                reply.add_auth(RR(rname=attack_domain, rtype=QTYPE.NS, rclass=1, ttl=TTL, rdata=rdata))
+                reply.add_answer(RR(rname=attack_domain, rtype=QTYPE.NS, rclass=1, ttl=TTL, rdata=rdata))
         else:
             reply.add_answer(RR(rname=qname, rtype=QTYPE.A, rclass=1, ttl=TTL, rdata=A(IP2)))
     print("---- Reply:\n", reply)
